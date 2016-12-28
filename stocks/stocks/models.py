6ee48d261fac84ccf244a1ce8e90f33a4b6834da
Dylan Richardson <DylanRichardson47@gmail.com>
2016-12-28 16:53:22 -0500

    Add requirements.txt before I lose track of python libs

--- stocks/stocks/models.py
+++ stocks/stocks/models.py
@@ -2,8 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Game(models.Model):
     start_date = models.DateField(auto_now=True)
     end_date = models.DateField(auto_now=False)
@@ -11,3 +13,6 @@ class Game(models.Model):
     principle = models.IntegerField(default=2000)
     players = models.ManyToManyField(User, related_name='players')
     creator = models.ForeignKey(User, related_name='creator')
+
+    def __str__(self):
+        return self.name
