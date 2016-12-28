79072a5b68356126a0071dad24196a469a68aba0
Dylan Richardson <DylanRichardson47@gmail.com>
2016-12-28 11:38:32 -0500

    Add Games DB object with correct relationships with Users from auth package

--- stocks/mysite/settings.py
+++ stocks/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'stocks.apps.StocksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'stocks.urls'
+ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'stocks.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
--- stocks/stocks/models.py
+++ stocks/stocks/models.py
@@ -1,5 +1,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Game(models.Model):
+    start_date = models.DateField(auto_now=True)
+    end_date = models.DateField(auto_now=False)
+    name = models.CharField(max_length=50)
+    principle = models.IntegerField(default=2000)
+    players = models.ManyToManyField(User, related_name='players')
+    creator = models.ForeignKey(User, related_name='creator')
